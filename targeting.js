e2ed5901e6c959139f4ad99b1eb8013817b5d3bc
Joshua Ordehi <joshua.ordehi@gmail.com>
2020-07-19 20:26:36 -0400

    Now able to take multiple URLs as input and displays a result for each URL

--- targeting.js
+++ targeting.js
@@ -4,42 +4,59 @@ const validURL = /[(http(s)?):\/\/(www\.)?a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6
 function check() {
     let selection = document.getElementById('rule-select').value;
     let rule = document.getElementById('rule').value;
-    let url = document.getElementById('url').value;
+    let urls = document.getElementById('url').value.split(/\n|\,/);
     
-    if (selection === 'simple' && validURL.test(rule) && validURL.test(url)) {
-        simplematch(rule, url);
-    } else if (selection === 'exact' && validURL.test(rule) && validURL.test(url)) {
-        exactmatch(rule, url);
-    } else if (selection === 'contains' && rule !== '' && url !== '') {
-        urlContains(rule, url);
-    } else if (selection === 'endswith' && rule !== '' && url !== '') {
-        urlEndswith(rule, url);
-    } else if (selection === 'startswith' && rule !== '' && url !== '') {
-        urlStartswith(rule, url);
+    if (selection === 'simple' && validURL.test(rule) && validURL.test(urls)) {
+        simplematch(rule, urls);
+    } else if (selection === 'exact' && validURL.test(rule) && validURL.test(urls)) {
+        exactmatch(rule, urls);
+    } else if (selection === 'contains' && rule !== '' && urls !== '') {
+        urlContains(rule, urls);
+    } else if (selection === 'endswith' && rule !== '' && urls !== '') {
+        urlEndswith(rule, urls);
+    } else if (selection === 'startswith' && rule !== '' && urls !== '') {
+        urlStartswith(rule, urls);
     } else {
-        invalidInput(rule, url);
+        invalidInput(rule, urls);
     }
   }
 
-function invalidInput(rule, url) {
+function invalidInput(rule, urls) {
     if (rule === "") {
         console.log("Please enter a targeting parameter.");
-    } else if (url === "") {
+    } else if (urls === "") {
         console.log("Please enter a URL to match.");
-    } else if (!validURL.test(rule) || !validURL.test(url)) {
+    } else if (!validURL.test(rule) || !validURL.test(urls)) {
         console.log(`Please enter valid URLs
 Format must be http://example.com`);
     }
 }
 
-function simplematch(rule, url) {
+function simplematch(rule, urls) {
     let protocol = /^https?:\/\//;
     let www = /www\./;
-    let params = /\#.*|\?.*|&.*|\/$/;
+    let params = /\#.*|\?.*|&.*|\/\#.*|\/\?.*/;
     rule = rule.replace(protocol, '').replace(www, '').replace(params, '');
-    url = url.replace(protocol, '').replace(www, '').replace(params, '');
-    let result = rule === url;
-    printResult(result); 
+    let cleanURLs = urls.map(function(url) {
+        url = url.replace(protocol, '').replace(www, '').replace(params, '');
+        return url;
+    });
+
+    let result = generateResultHTML(cleanURLs);
+    
+    function generateResultHTML() {
+        let resultHTML = `<ul>`;
+        for (let i = 0; i < cleanURLs.length; i++) {
+            if (rule === cleanURLs[i]) {
+                resultHTML += `<li class="match">✅ ${urls[i]}</li>`;
+            } else {
+                resultHTML += `<li class="notamatch">❌ ${urls[i]}</li>`;
+            }
+        }
+        resultHTML += `</ul>`;
+        return resultHTML;
+    }
+    resultDiv.innerHTML = result;
 }
 
 
@@ -47,23 +64,23 @@ function simplematch(rule, url) {
 //     log(simplematch(urls[i]));
 // }
 
-function exactmatch(rule, url) {
-    let result = rule.toLowerCase() === url.toLowerCase();
+function exactmatch(rule, urls) {
+    let result = rule.toLowerCase() === urls.toLowerCase();
     printResult(result);
 }
 
-function urlContains(rule, url) {
-    let result = url.toLowerCase().includes(rule.toLowerCase());
+function urlContains(rule, urls) {
+    let result = urls.toLowerCase().includes(rule.toLowerCase());
     printResult(result);
 }
 
-function urlStartswith(rule, url) {
-    let result = url.toLowerCase().startsWith(rule.toLowerCase());
+function urlStartswith(rule, urls) {
+    let result = urls.toLowerCase().startsWith(rule.toLowerCase());
     printResult(result);
 }
 
-function urlEndswith(rule, url) {
-    let result = url.toLowerCase().endsWith(rule.toLowerCase());
+function urlEndswith(rule, urls) {
+    let result = urls.toLowerCase().endsWith(rule.toLowerCase());
     printResult(result);
 }
 
